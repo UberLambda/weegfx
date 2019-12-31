1567b72540c8f817e792a8dfd3f612c6b8a2ecac
Paolo Jovon <paolo.jovon@gmail.com>
2019-12-31 00:12:26 +0000

    Implemented most of the STM32 backend
    It has DMA support, but otherwise lets the user choose how to send
    address window commands, how to assert/deassert CS and other signal
    lines and - in general - how to handle all communication to the screen.

--- backends/weegfx_stm32.c
+++ backends/weegfx_stm32.c
@@ -2,3 +2,74 @@
 // Copyright (c) 2019 Paolo Jovon <paolo.jovon@gmail.com>
 // Released under the 3-clause BSD license (see LICENSE)
 #include "weegfx_stm32.h"
+
+// -- "Inspired" by https://vivonomicon.com/2019/07/05/bare-metal-stm32-programming-part-9-dma-megamix/ --
+
+/// Maximum number of bytes that can be sent at once over DMA.
+#define DMA_MAX_TRANSFER_SIZE 0xFFFF
+
+int wgfxSTM32Init(WGFXstm32Backend *self, unsigned priority)
+{
+    if(!(self->spi && self->dma && self->dmaChannel && self->beginScreenWrite && self->endScreenWrite))
+    {
+        return 0;
+    }
+    if(priority > 0x3) priority = 0x3;
+
+    // 8/16-bit memory size, 8/16-bit peripheral size, increment memory pointer but not peripheral, memory -> peripheral
+    const unsigned dmaSize = (self->bpp % 2) == 0 ? 0x1 : 0x0; // Either 8 or 16 bits
+    self->dmaChannel->CCR = 0x00000000;
+    self->dmaChannel->CCR |= (priority << DMA_CCR_PL_Pos) | (dmaSize << DMA_CCR_MSIZE_Pos) | (dmaSize << DMA_CCR_PSIZE_Pos) | DMA_CCR_PINC | DMA_CCR_DIR;
+
+    // Destination = the SPI data register
+    self->dmaChannel->CPAR = (WGFX_U32)&self->spi->DR;
+
+    // Clear pending transfer complete/error bits
+    self->dma->ISR |= self->dmaISRDoneMask;
+
+    return 1;
+}
+
+/// Transfer a buffer to SPI via DMA.
+WGFX_FORCEINLINE void dmaSpiTx(WGFXstm32Backend *self, const void *buf, WGFX_SIZET size)
+{
+    while(self->dma->ISR & self->dmaISRDoneMask) {}
+    self->dma->ISR |= self->dmaISRDoneMask;
+
+    self->dmaChannel->CNDTR = size;
+    self->dmaChannel->CMAR = (WGFX_U32)buf;
+    self->dmaChannel->CCR |= DMA_CCR_EN;
+}
+
+void wgfxSTM32Write(const WGFX_U8 *buf, WGFX_SIZET size, void *userPtr)
+{
+    WGFXstm32Backend *self = (WGFXstm32Backend *)userPtr;
+
+    WGFX_SIZET i;
+    for(i = DMA_MAX_TRANSFER_SIZE; i < size; i += DMA_MAX_TRANSFER_SIZE)
+    {
+        dmaSpiTx(self, buf, DMA_MAX_TRANSFER_SIZE);
+    }
+    if((size - (i - DMA_MAX_TRANSFER_SIZE)) > 0)
+    {
+        dmaSpiTx(self, buf, size - i);
+    }
+}
+
+void wgfxSTM32BeginWrite(unsigned x, unsigned y, unsigned w, unsigned h, void *userPtr)
+{
+    WGFXstm32Backend *self = (WGFXstm32Backend *)userPtr;
+    if(self->beginScreenWrite)
+    {
+        self->beginScreenWrite(x, y, w, h, self->backendUserPtr);
+    }
+}
+
+void wgfxSTM32EndWrite(void *userPtr)
+{
+    WGFXstm32Backend *self = (WGFXstm32Backend *)userPtr;
+    if(self->endScreenWrite)
+    {
+        self->endScreenWrite(self->backendUserPtr);
+    }
+}
--- backends/weegfx_stm32.h
+++ backends/weegfx_stm32.h
@@ -5,7 +5,7 @@
 #define WEEGFX_STM32_H
 
 #ifndef WEEGFX_H
-#include <weegfx.h>
+#    include <weegfx.h>
 #endif
 
 #define WGFX_ANGLED(name) <name>
@@ -19,7 +19,7 @@
 // ```
 // before including this header.
 // Note that header filenames are case-sensitive on Linux!
-#error "No WGFX_STM32_DEVICE defined!"
+#    error "No WGFX_STM32_DEVICE defined!"
 #endif
 #include WGFX_ANGLED(WGFX_STM32_DEVICE.h)
 
@@ -27,7 +27,65 @@
 extern "C" {
 #endif
 
-// TODO
+/// Called before writing pixel data to the screen.
+typedef void (*WGFXstm32BeginScreenWritePFN)(unsigned x, unsigned y, unsigned w, unsigned h, void *backendUserPtr);
+
+/// Called after the pixel data is written to the screen.
+typedef void (*WGFXstm32EndScreenWritePFN)(void *backendUserPtr);
+
+/// An instance of the STM32 backend for weegfx.
+typedef struct
+{
+    /// The bytes per pixel to transfer.
+    unsigned bpp;
+
+    /// The SPI to use for sending pixel data.
+    SPI_TypeDef *spi;
+
+    /// The DMA used to copy pixel data from memory to SPI.
+    DMA_TypeDef *dma;
+
+    /// The DMA channel in `dma` used to copy pixel data from memory to SPI.
+    DMA_Channel_TypeDef *dmaChannel;
+
+    /// `(1 << DMA_ISR_TCIFx_Pos) | (1 << DMA_ISR_TEIFx)` for x = index of `dmaChannel`;
+    /// ORed with `dma`'s `ISR` to know when a write is complete.
+    WGFX_U32 dmaISRDoneMask;
+
+    /// Called before pixel data for the given rect is written to the screen via SPI. Can be null.
+    ///
+    /// Use this to assert the CS pin, set the address window and prepare the screen for receiving data.
+    WGFXstm32BeginScreenWritePFN beginScreenWrite;
+
+    /// Called after pixel data has been written to the screen via SPI. Can be null.
+    ///
+    /// Use this to deassert the CS pin.
+    WGFXstm32EndScreenWritePFN endScreenWrite;
+
+    /// User pointer, passed as-is to `beginScreenWrite` and `endScreenWrite`.
+    void *backendUserPtr;
+} WGFXstm32Backend;
+
+/// Initializes the DMA channel at `self->dma` so that it will transfer data to
+/// `self->spi` (this means that `self` should already have been populated by the user!)
+/// `priority` is the priority to set for the DMA channel (0 to 3).
+/// Returns false on error.
+///
+/// Note that this does NOT enable the DMA, or try to change the configuration of the SPI!
+/// (don't forget to enable the peripherals, set `TXDMAEN` in the SPI's `CR2` and/or DMA remap bits in `SYSCFG`!)
+int wgfxSTM32Init(WGFXstm32Backend *self, unsigned priority);
+
+/// The `beginWrite` implementation for STM32.
+/// Set the `userPtr` of the `WGFXscreen` to point to a valid `WGFXstm32Backend`!
+void wgfxSTM32BeginWrite(unsigned x, unsigned y, unsigned w, unsigned h, void *userPtr);
+
+/// The `write` implementation for STM32.
+/// Set the `userPtr` of the `WGFXscreen` to point to a valid `WGFXstm32Backend`!
+void wgfxSTM32Write(const WGFX_U8 *buf, WGFX_SIZET size, void *userPtr);
+
+/// The `endWrite` implementation for STM32.
+/// Set the `userPtr` of the `WGFXscreen` to point to a valid `WGFXstm32Backend`!
+void wgfxSTM32EndWrite(void *userPtr);
 
 #ifdef __cplusplus
 }
